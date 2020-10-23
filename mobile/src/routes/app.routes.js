d674dbb413465519941b96ed1841b68ce58d5807
enriqueizel2 <eleaoizel@gmail.com>
2020-10-22 21:36:50 -0400

    feat:  criando rotas de navegação de pilha
    A nova rota é entre as telas Home e RoomDetails

--- mobile/src/routes/app.routes.js
+++ mobile/src/routes/app.routes.js
@@ -1,15 +1,35 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Home from "../screens/Home";
 import About from "../screens/About";
+import RoomDetails from "../screens/RoomDetails";
 
 const AppDrawer = createDrawerNavigator();
+const AppStack = createStackNavigator();
+
+const HomeNavigator = () => {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <AppStack.Screen
+        name="RoomDetails"
+        component={RoomDetails}
+        options={{ headerStyle: { backgroundColor: "#3C896D" } }}
+      />
+    </AppStack.Navigator>
+  );
+};
 
 const AppRoutes = () => {
   return (
     <AppDrawer.Navigator>
-      <AppDrawer.Screen name="Home" component={Home} />
+      <AppDrawer.Screen name="HomeNavigator" component={HomeNavigator} />
       <AppDrawer.Screen name="About" component={About} />
     </AppDrawer.Navigator>
   );
