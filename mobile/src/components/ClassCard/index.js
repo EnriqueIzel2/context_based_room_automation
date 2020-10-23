9cd558a1ea885b7339091f0b8e48637ea44bb90a
enriqueizel2 <eleaoizel@gmail.com>
2020-10-22 21:37:42 -0400

    feat: alterando a View do card para TouchableOpacity

--- mobile/src/components/ClassCard/index.js
+++ mobile/src/components/ClassCard/index.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
-import { Container, TextBase, TextBold } from "./styles";
+import { TouchableContainer, TextBase, TextBold } from "./styles";
 
 const ClassCard = ({ data }) => {
+  const navigation = useNavigation();
+
+  function handleNavigate() {
+    navigation.navigate("RoomDetails");
+  }
+
   return (
-    <Container status={data.status}>
+    <TouchableContainer status={data.status} onPress={handleNavigate}>
       <TextBase>Nome da disciplina: {data.title}</TextBase>
       <TextBase>Sala de aula</TextBase>
       <TextBold>Horário da aula</TextBold>
       <TextBold>Status da aula: {data.status}</TextBold>
-    </Container>
+    </TouchableContainer>
   );
 };
 
