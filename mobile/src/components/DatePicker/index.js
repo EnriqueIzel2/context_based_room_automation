18b10d85a446ebeda7052b85cc222ea69c2bf5b7
enriqueizel2 <eleaoizel@gmail.com>
2020-10-23 18:27:20 -0400

    Removendo funções do componente

--- mobile/src/components/DatePicker/index.js
+++ mobile/src/components/DatePicker/index.js
@@ -1,25 +1,18 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 const DatePicker = ({ date, onChangeDate }) => {
-  const [dateNow, setDateNow] = useState(new Date(date));
-
   return (
-    <View>
+    <Container>
       <DateTimePicker
-        value={dateNow}
+        value={date}
         mode="date"
         display="default"
-        onChange={(event, selectedDate) => {
-          const currentDate = selectedDate || dateNow;
-          setDateNow(currentDate);
-          onChangeDate(currentDate);
-        }}
+        onChange={onChangeDate}
       />
-    </View>
+    </Container>
   );
 };
 
