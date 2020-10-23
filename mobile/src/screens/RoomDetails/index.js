7cdafb7d007031ce821628b0bbbd62f9bb293e2a
enriqueizel2 <eleaoizel@gmail.com>
2020-10-22 21:40:16 -0400

    feat: criação da tela RoomDetails

---
+++ mobile/src/screens/RoomDetails/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { Background } from "../../components/Background";
+// import { Container } from './styles';
+
+const RoomDetails = () => {
+  return (
+    <Background>
+      <Text>Tela de detalhes da aula</Text>
+    </Background>
+  );
+};
+
+export default RoomDetails;
